3571e273fd3ce293d2a0d737a1754df368676120
Junge21 <prajwolk18@gmail.com>
2019-01-22 09:43:53 +0545

    Create arithmetic.php

---
+++ arithmetic.php
@@ -0,0 +1,21 @@
+<<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Testing Arithmetic Opertors</title>
+
+</head>
+<body>
+    <center><h3 style="color:blue">Testing Arithmetic Opertors</h3></center>
+        <?php
+        function addition($i,$j){
+            $sum= $i+$j;
+            echo "<p> The sum of $i and $j is: $sum<p>";
+        }
+        $a=25;
+        $b=75;
+        addition($a,$b);
+
+?>
+</body>
+</html>
