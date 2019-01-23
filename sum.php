120d33a84bb2ef68fbcf0002e498e395309c7df2
AbhijeetSah <abhijeet.sah5895@gmail.com>
2019-01-23 08:44:17 +0545

    isprime function has added (#5)

--- sum.php
+++ sum.php
@@ -16,9 +16,39 @@ return $sum;
 // Driver code 
 $n = 5; 
 echo findSum($n); 
+
+if($_Server["REQUEST_METHOD"]=="POST"){
+
+    $num=$_POST["TestNumber"];
+    IsPrime($num);
+
+}
+function IsPrime($num){
+    $count=0;
+    for($i=2; $i<$num/2; $i++){
+        if($num%$i==0){
+            $count++;
+            break;
+        }
+        else{
+            continue;
+        }
+    }
+    if($num=="0" || $num=="1"){
+        echo "<br>neither prime nor composit";
+
+    }
+    elseif($count==1){
+        echo "<br>number is composit";
+    }
+    else{
+        echo "<br>number is prime";
+    }
+}
   
 // This code is contributed by Sam007 
 ?> 
+
 
 
 <?php
